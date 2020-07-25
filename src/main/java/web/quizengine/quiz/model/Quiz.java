007358affd74ec78f319fde344491d42589767b9
sergey <sergei.ni@yandex.ru>
2020-07-26 00:07:53 +0300

    QuizEntity

--- src/main/java/web/quizengine/quiz/model/Quiz.java
+++ src/main/java/web/quizengine/quiz/model/Quiz.java
@@ -1,31 +1,32 @@
 package web.quizengine.quiz.model;
 
+
+import com.sun.istack.NotNull;
+import lombok.*;
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "quiz")
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
 public class Quiz {
+    @Id
+    @Column
+    @GeneratedValue
+    private Long id;
+    @NotNull
+    @Column
     String title;
+    @NotNull
+    @Column
     String text;
-    String[] options;
 
-    public String getTitle() {
-        return title;
-    }
+    @Column
+    @NotNull
+    @ElementCollection
+    private List<String> options;
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
-
-    public String[] getOptions() {
-        return options;
-    }
-
-    public void setOptions(String[] options) {
-        this.options = options;
-    }
 }
