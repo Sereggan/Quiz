ba32cc0ea9a8fe27aac914df5a6730d47c935d12
sergey <sergei.ni@yandex.ru>
2020-07-26 00:07:40 +0300

    QuizController

--- src/main/java/web/quizengine/quiz/controllers/QuizController.java
+++ src/main/java/web/quizengine/quiz/controllers/QuizController.java
@@ -1,10 +1,32 @@
 package web.quizengine.quiz.controllers;
 
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import web.quizengine.quiz.model.Quiz;
 
-@Controller
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
 @RequestMapping("/api/quizzes")
 public class QuizController {
+    private List<Quiz> quizList = new ArrayList<>();
+
+    public QuizController() {
+       List<String> options = Arrays.asList("option1", "option 2");
+        quizList.add(new Quiz(0L,"Test 1","Text 1",options));
+    }
+
+    @GetMapping
+    public Quiz getQuiz() {
+        return quizList.get(0);
+    }
+
+    @PostMapping
+    public ResponseEntity<Quiz> addQuiz(@RequestBody Quiz quiz){
+        
+    }
 }
