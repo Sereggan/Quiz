dd3f9505e75e46cf5e02fc63a149bb8a6e661b1b
sergey <sergei.ni@yandex.ru>
2020-07-29 00:12:54 +0300

    added answers

--- backend/src/main/java/web/quizengine/quiz/model/Quiz.java
+++ backend/src/main/java/web/quizengine/quiz/model/Quiz.java
@@ -4,7 +4,9 @@ package web.quizengine.quiz.model;
 import com.sun.istack.NotNull;
 import lombok.*;
 import javax.persistence.*;
+import javax.validation.constraints.Size;
 import java.util.List;
+import java.util.Objects;
 
 @Entity
 @Table(name = "quiz")
@@ -17,6 +19,7 @@ public class Quiz {
     @Column
     @GeneratedValue
     private Long id;
+
     @NotNull
     @Column
     String title;
@@ -25,8 +28,27 @@ public class Quiz {
     String text;
 
     @Column
-    @NotNull
+    @Size(min = 2,max = 4)
     @ElementCollection
     private List<String> options;
 
+    @Column
+    @Size(min=1)
+    @ElementCollection
+    private List<Integer> answer;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Quiz)) return false;
+        Quiz quiz = (Quiz) o;
+        return getTitle().equals(quiz.getTitle()) &&
+                getText().equals(quiz.getText()) &&
+                getOptions().equals(quiz.getOptions());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getTitle(), getText(), getOptions());
+    }
 }
