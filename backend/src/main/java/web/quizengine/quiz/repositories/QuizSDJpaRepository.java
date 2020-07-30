e4c33f70a55ddf823bc393f8ac3b0164dc3fb539
sergey <sergei.ni@yandex.ru>
2020-07-31 00:50:52 +0300

    Going to add paging

--- backend/src/main/java/web/quizengine/quiz/repositories/QuizSDJpaRepository.java
+++ backend/src/main/java/web/quizengine/quiz/repositories/QuizSDJpaRepository.java
@@ -2,10 +2,11 @@ package web.quizengine.quiz.repositories;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 import web.quizengine.quiz.model.Quiz;
 
 @Qualifier("springData")
 @Repository
-public interface QuizSDJpaRepository extends CrudRepository<Quiz, Long> {
+public interface QuizSDJpaRepository extends PagingAndSortingRepository<Quiz, Long> {
 }
