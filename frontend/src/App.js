9e7c286806cab1c016bfd66a1a34c5d4b4901883
sergey <sergei.ni@yandex.ru>
2020-07-31 00:51:20 +0300

    Routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,23 @@
 import React from "react";
+
+import { BrowsRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
+import ListQuizzesComponent from "./components/ListQuizzesComponent";
+import CreateQuiz from "./components/CreateQuiz";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <Router>
+      <div className="container">
+        <Switch>
+          <Route exact path="/" component={ListQuizzesComponent} />
+          <Route path="/quizzes" component={ListQuizzesComponent} />
+          <Route path="/add-quiz" component={CreateQuiz} />
+          <Redirect to="/" />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
